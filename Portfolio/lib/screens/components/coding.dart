1643715af1c35f7906eecfd6ac164c4c32538e3f
mensur056 <mansur.sarxanov@gmail.com>
2021-11-23 14:51:24 +0400

    test

--- Portfolio/lib/screens/components/coding.dart
+++ Portfolio/lib/screens/components/coding.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:portfilo/constants.dart';
 
+import '../../constants.dart';
+
 class Coding extends StatelessWidget {
   const Coding({
     Key? key,
@@ -8,16 +10,29 @@ class Coding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Divider(),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical:defaultPadding),
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
           child: Text(
             'Coding',
             style: Theme.of(context).textTheme.subtitle2,
           ),
-        )
+        ),
+        TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: 0.8),
+            duration: defaultDuration,
+            builder: (context, double value, child) => Column(
+              children: [
+                LinearProgressIndicator(
+                      value: value,
+                      color: primaryColor,
+                      backgroundColor: darkColor,
+                    ),
+              ],
+            ))
       ],
     );
   }
