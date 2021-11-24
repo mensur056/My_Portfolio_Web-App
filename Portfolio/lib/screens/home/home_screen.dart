cfc4e6fd916610729f7d772bc7c4caea5faeb8ae
mensur056 <mansur.sarxanov@gmail.com>
2021-11-24 21:46:53 +0400

    test

--- Portfolio/lib/screens/home/home_screen.dart
+++ Portfolio/lib/screens/home/home_screen.dart
@@ -1,9 +1,9 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:portfilo/constants.dart';
 import 'package:portfilo/screens/main_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +20,66 @@ class HomeScreen extends StatelessWidget {
               ),
               Container(
                 color: darkColor.withOpacity(0.66),
-              ),Column(children: [Text('Discover My Amazing Art Space!',style: TextStyle(color: Colors.white,fontSize: 45,fontWeight: FontWeight.bold))],)
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Discover My Amazing \nArt Space!',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold)),
+                    DefaultTextStyle(
+                      style: Theme.of(context).textTheme.subtitle!,
+                      child: Row(
+                        children: [
+                          FlutterCodeText(),
+                          SizedBox(
+                            width: defaultPadding,
+                          ),
+                          Text('I build'),
+                          AnimatedTextKit(
+                            animatedTexts: [
+                              TyperAnimatedText(' web and mobile app',speed: Duration(milliseconds: 60)),
+                              TyperAnimatedText(' online and offline app',speed: Duration(milliseconds: 60)),
+                            ],
+                          ),SizedBox(
+                            width: defaultPadding,
+                          ),FlutterCodeText(),
+
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         )
       ],
+    );
+  }
+}
+
+class FlutterCodeText extends StatelessWidget {
+  const FlutterCodeText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(text: '<', children: [
+        TextSpan(
+            text: 'flutter',
+            style: TextStyle(
+              color: primaryColor,
+            )),
+        TextSpan(text: '>')
+      ]),
     );
   }
 }
