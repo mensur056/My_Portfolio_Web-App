7dc9198575b124b7b703645ae241ee66622ae0d7
mensur056 <mansur.sarxanov@gmail.com>
2021-11-24 17:52:34 +0400

    test

--- Portfolio/lib/screens/home/home_screen.dart
+++ Portfolio/lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfilo/constants.dart';
 import 'package:portfilo/screens/main_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,16 +7,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainScreen(children: [
-      AspectRatio(
-        aspectRatio: 3,
-        child: Stack(fit: StackFit.expand, children: [
-          Image.asset(
-            'images2/bg.jpeg',
-            fit: BoxFit.cover,
-          )
-        ]),
-      )
-    ]);
+    return MainScreen(
+      children: [
+        AspectRatio(
+          aspectRatio: 3,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset(
+                'images2/1136157.png',
+                fit: BoxFit.cover,
+              ),
+              Container(
+                color: darkColor.withOpacity(0.66),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
