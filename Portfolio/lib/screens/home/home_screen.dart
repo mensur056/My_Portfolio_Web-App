06041770ede79819d2e2d664e4e8fea49275e9c0
mensur056 <mansur.sarxanov@gmail.com>
2021-11-25 14:36:34 +0400

    test

--- Portfolio/lib/screens/home/home_screen.dart
+++ Portfolio/lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfilo/constants.dart';
+import 'package:portfilo/models/project.dart';
 import 'package:portfilo/screens/components/my_build_animated_text.dart';
 import 'package:portfilo/screens/home/home_banner.dart';
 import 'package:portfilo/screens/main_screen.dart';
@@ -10,11 +11,38 @@ class HomeScreen extends StatelessWidget {
     return MainScreen(
       children: [
         HomeBanner(),
+        SizedBox(
+          height: defaultPadding,
+        ),
         Text(
           'My Project',
           style: Theme.of(context).textTheme.headline6,
         ),
-
+        SizedBox(
+          height: defaultPadding,
+        ),
+        GridView.builder(
+          itemCount: demo_projects.length,
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 1.3,
+              crossAxisSpacing: defaultPadding,
+              mainAxisSpacing: defaultPadding),
+          itemBuilder: (context, index) => Container(
+            padding: EdgeInsets.all(defaultPadding),
+            color: secondaryColor,
+            child: Column(
+              children: [
+                Text(
+                  demo_projects[0].title!,
+                  maxLines: 2,
+                  style: Theme.of(context).textTheme.subtitle2,
+                )
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
