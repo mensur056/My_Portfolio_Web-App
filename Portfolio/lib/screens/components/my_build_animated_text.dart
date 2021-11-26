f2a42ace49e3538c5ffa79448e44e140a4101781
mensur056 <mansur.sarxanov@gmail.com>
2021-11-26 16:55:56 +0400

    test

--- Portfolio/lib/screens/components/my_build_animated_text.dart
+++ Portfolio/lib/screens/components/my_build_animated_text.dart
@@ -5,28 +5,35 @@ import 'package:portfilo/responsive.dart';
 import 'package:portfilo/screens/components/flutter_code_text.dart';
 
 class MyBuildAnimatedText extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
       style: Theme.of(context).textTheme.subtitle!,
       child: Row(
         children: [
-          if(!Responsive.isMobileLarge(context))FlutterCodeText(),
-          SizedBox(
-            width: defaultPadding,
-          ),
+          if (!Responsive.isMobileLarge(context)) FlutterCodeText(),
+          if (!Responsive.isMobileLarge(context))
+            SizedBox(
+              width: defaultPadding,
+            ),
           Text('I build'),
           AnimatedTextKit(
             animatedTexts: [
-              TyperAnimatedText(' web and mobile app',speed: Duration(milliseconds: 80,)),
-              TyperAnimatedText(' online and offline app',speed: Duration(milliseconds: 80,)),
+              TyperAnimatedText(' web and mobile app',
+                  speed: Duration(
+                    milliseconds: 80,
+                  )),
+              TyperAnimatedText(' online and offline app',
+                  speed: Duration(
+                    milliseconds: 80,
+                  )),
             ],
-          ),SizedBox(
-            width: defaultPadding,
-          ),FlutterCodeText(),
-
+          ),
+          if (!Responsive.isMobileLarge(context))
+            SizedBox(
+              width: defaultPadding,
+            ),
+          if (!Responsive.isMobileLarge(context)) FlutterCodeText(),
         ],
       ),
     );
